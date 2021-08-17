b5cd82680db9bbb99386419cfc961fab5229f699
Sanjay Chaturvedi <sanjay.chaturvedi@dotsquares.com>
2021-08-17 18:12:59 +0530

    code updated

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import stanford_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -47,11 +47,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'stanford_theme'
+html_theme_path = [stanford_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
 master_doc = 'index'
